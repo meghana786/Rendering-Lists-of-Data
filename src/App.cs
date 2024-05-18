d9efe1cd2dab69d8f7c6df28cdebc8dbc39cfb57
meghana786 <160600002+meghana786@users.noreply.github.com>
2024-05-18 15:57:37 +0530

    Create App.cs

---
+++ src/App.cs
@@ -0,0 +1,17 @@
+.list-elements {
+  color: blue;
+  text-align: center;
+  margin: 5px 0;
+}
+
+.list-elements span {
+  margin-right: 30px; 
+}
+
+.list-group {
+  border: 2px solid black;
+}
+h1{
+  text-align: center;
+  font-size: 20px;
+}
